62a18e8c3300346f3401d2dbdf0e9a6f4d2c88c9
kief <kmorris@thoughtworks.com>
2016-02-29 15:57:44 +0000

    Cleaned up the S3 bucket uploading, added policies

--- upload.sh
+++ upload.sh
@@ -5,6 +5,8 @@ jekyll build
 aws s3 sync \
   --acl public-read \
   --exact-timestamps \
+  --profile infrasite \
   --delete \
+  --region eu-west-1 \
   ./_site/ \
   s3://infrastructure-as-code.com/
